cbfd7a1075fee66c4d7f4b7ead95bf41f6eceea5
abhishek-shivale <abhishekshivale45@gmail.com>
2025-03-29 22:36:34 +0530

    [FEAT]: added commit lint

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,21 +1,11 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller, Get } from '@nestjs/common';
+// import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  // constructor(private readonly authService: AuthService) {}
   @Get()
   getHello(): string {
     return 'Hello World!';
-  }
-
-  @Get()
-  fetchUser() {
-    return 'hello';
-  }
-
-  @Post()
-  fetchUsers() {
-    return 'hello';
   }
 }
